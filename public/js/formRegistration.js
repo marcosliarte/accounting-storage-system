40657440042fe9547d05cacca7f55146c3b7ab4d
marcosliarte <58516645+marcosliarte@users.noreply.github.com>
2024-07-22 14:50:53 -0400

    validation CPF

--- public/js/formRegistration.js
+++ public/js/formRegistration.js
@@ -1,6 +1,6 @@
 // Função para validar CNPJ
 function validarCnpj(cnpj) {
-    cnpj = cnpj.replace(/[^\d]+/g,'');
+    cnpj = cnpj.replace(/[^\d]+/g, '');
 
     if (cnpj == '') return false;
 
@@ -51,9 +51,58 @@ function validarCnpj(cnpj) {
     return true;
 }
 
-// Adiciona a máscara de CNPJ e evento de validação
+// Função para validar CPF
+function validarCpf(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf == '') return false;
+
+    if (cpf.length != 11)
+        return false;
+
+    if (cpf == "00000000000" || 
+        cpf == "11111111111" || 
+        cpf == "22222222222" || 
+        cpf == "33333333333" || 
+        cpf == "44444444444" || 
+        cpf == "55555555555" || 
+        cpf == "66666666666" || 
+        cpf == "77777777777" || 
+        cpf == "88888888888" || 
+        cpf == "99999999999")
+        return false;
+
+    // Valida DVs
+    let tamanho = cpf.length - 2;
+    let numeros = cpf.substring(0, tamanho);
+    let digitos = cpf.substring(tamanho);
+    let soma = 0;
+
+    // Calcula o primeiro dígito verificador
+    for (let i = 10; i > 1; i--) {
+        soma += numeros.charAt(10 - i) * i;
+    }
+    let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0)) 
+        return false;
+
+    // Calcula o segundo dígito verificador
+    tamanho = tamanho + 1;
+    numeros = cpf.substring(0, tamanho);
+    soma = 0;
+    for (let i = 11; i > 1; i--) {
+        soma += numeros.charAt(11 - i) * i;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+        return false;
+
+    return true;
+}
+
+// Adiciona a máscara e evento de validação para CNPJ e CPF
 $(document).ready(function(){
     $('#CNPJ').mask('00.000.000/0000-00');
+    $('#CPF').mask('000.000.000-00');
 
     $('#CNPJ').on('blur', function() {
         const cnpj = this.value;
@@ -64,6 +113,19 @@ $(document).ready(function(){
             this.style.borderColor = 'red';
         } else {
             cnpjError.textContent = '';
+            this.style.borderColor = '';
+        }
+    });
+
+    $('#CPF').on('blur', function() {
+        const cpf = this.value;
+        const isValid = validarCpf(cpf);
+        const cpfError = document.getElementById('cpfError');
+        if (!isValid) {
+            cpfError.textContent = 'CPF inválido.';
+            this.style.borderColor = 'red';
+        } else {
+            cpfError.textContent = '';
             this.style.borderColor = '';
         }
     });
